c92de709a2fac1d039f6c5d6021e7980c91b6b3d
DeepankerProjects <deepankerkumar94@gmail.com>
2019-09-12 13:43:03 +0530

    Updates

---
+++ precache-manifest.f23744bb781a9b80c464b3deeafd53e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e66e87a6414728899e",
+    "url": "/my-resume-app/static/js/runtime~main.82e66e87.js"
+  },
+  {
+    "revision": "b24a218194b1636dca03",
+    "url": "/my-resume-app/static/js/main.b24a2181.chunk.js"
+  },
+  {
+    "revision": "8f0e20bbe6997599a7b6",
+    "url": "/my-resume-app/static/js/2.8f0e20bb.chunk.js"
+  },
+  {
+    "revision": "b24a218194b1636dca03",
+    "url": "/my-resume-app/static/css/main.62604e4e.chunk.css"
+  },
+  {
+    "revision": "8f0e20bbe6997599a7b6",
+    "url": "/my-resume-app/static/css/2.32714345.chunk.css"
+  },
+  {
+    "revision": "07463f79c64e733cc178fe285023c032",
+    "url": "/my-resume-app/index.html"
+  }
+];
